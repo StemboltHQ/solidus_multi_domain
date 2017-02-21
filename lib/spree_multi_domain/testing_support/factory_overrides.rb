8d4ffa3ea2fa7f0d0eb668d72d34b55d373c038b
Luuk Veenis <luuk@stembolt.com>
2017-02-21 11:37:30 -0800

    Access product through variant explicitly
    In Solidus 1.x there was a `delegate :product, to: :variant` on
    Spree::LineItem. In 2.x this was removed, since there is already a
    `has_one :product, through: :variant` on LineItem's.
    
    This change causes issues in specs where we use the build strategy for
    line_items because line_item.product fails because the variant_id on the
    line_item is still nil. If we reference it explicitly, we can access the
    product without first saving the record.

--- lib/spree_multi_domain/testing_support/factory_overrides.rb
+++ lib/spree_multi_domain/testing_support/factory_overrides.rb
@@ -2,7 +2,7 @@ FactoryGirl.modify do
   # Products need to belong to the same store as the order.
   factory :line_item do
     after(:build) do |line_item, evaluator|
-      if line_item.order.store && line_item.product.stores.empty?
+      if line_item.order.store && line_item.variant.product.stores.empty?
         line_item.product.stores << line_item.order.store
       end
     end
